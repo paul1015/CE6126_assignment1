454456c3f7a19d8ea167434d8aa323055d419c54
chungpaul <chungpaul1015@gmail.com>
2020-03-28 15:16:38 +0800

    car_move

--- code/code.py
+++ code/code.py
@@ -1,51 +1,114 @@
 from tkinter import *
+import tkinter as tkinter
+import math as math
 import time
 
-def create_line(x1,y1,x2,y2,color):
-    canvas.create_line(first_x+x1*multiply_x,first_y+(y1*-1)*multiply_y,first_x+x2*multiply_x,first_y+(y2*-1)*multiply_y, fill=color, width=3)
+#圖片的 軸是從左上角開始 x 是正 y 是負 
+#theta car_angle
 
-def create_circle(x, y, r, color): #center coordinates, radius
-    x0 = x - r*multiply_x
-    y0 = y - r*multiply_y
-    x1 = x + r*multiply_x
-    y1 = y + r*multiply_y
-    return canvas.create_oval(x0, y0, x1, y1, fill=color)
+class car:
+    def __init__(self, zero_x, zero_y, multiply_x, multiply_y, car_x, car_y, car_r=3, car_collar="red", car_angle = 90):
+        self.zero_x = zero_x
+        self.zero_y = zero_y
+        self. multiply_x = multiply_x
+        self. multiply_y = multiply_y
+        self.car_x = car_x
+        self.car_y = car_y
+        self.car_r = car_r
+        self.car_collar = car_collar
+        self.car_angle = round(math.radians(car_angle),2)
 
 
-tk = Tk()  # 画布
-tk.title('Momentum')
-tk.resizable(0, 0)  # 固定窗口大小
-width = 1000
-height = 1000
+    def create_line(self, x1, y1, x2, y2, color):
+        canvas.create_line(zero_x+x1*multiply_x, zero_y+(y1*-1)*self.multiply_y,
+                           zero_x+x2*multiply_x, zero_y+(y2*-1)*self.multiply_y, fill=color, width=3)
+
+    def create_circle(self):  
+        # center coordinates, radius
+        x0 = self.car_x - self.car_r*self.multiply_x
+        y0 = self.car_y - self.car_r*self.multiply_y
+        x1 = self.car_x + self.car_r*self.multiply_x
+        y1 = self.car_y + self.car_r*self.multiply_y
+        return canvas.create_oval(x0, y0, x1, y1, fill=self.car_collar)
+
+
+    def car_move(self, tk, car, theta):
+
+        #角度換算
+        theta = round(math.radians(theta),2)
+        # cos(a + b) = cos(a) * cos(b) - sin(a) * sin(b) 
+        # sin(a + b) = sin(a) * cos(b) +   sin(b) * cos(a) 
+        print(' t car_ x = ', self.car_x, 'car_y = ', self.car_y, 'car_angle', self.car_angle)
+        #self.car_x = self.car_x + math.cos(self.car_angle + theta) + (math.sin(theta) * math.sin(self.car_angle))
+        #self.car_y = self.car_y - (math.sin(self.car_angle + theta) - (math.sin(theta)) * math.cos(self.car_angle))
+        #self.car_angle = self.car_angle - math.asin(2 * math.sin(theta) / self.car_r)
+        self.car_x = self.car_x + 10*(math.cos(self.car_angle) * math.cos(theta)) 
+        
+        self.car_y = self.car_y -  10*(math.sin(self.car_angle) * math.cos(theta))
+        self.car_angle = self.car_angle - math.asin(2 * math.sin(theta) / self.car_r)
+
+        print(' t+1 car_ x = ', self.car_x, 'car_y = ', self.car_y, 'car_angle', self.car_angle)
+    
+        x0 = self.car_x - self.car_r*self.multiply_x
+        y0 = self.car_y - self.car_r*self.multiply_y
+        x1 = self.car_x + self.car_r*self.multiply_x
+        y1 = self.car_y + self.car_r*self.multiply_y
+        tk.coords(car, (x0, y0, x1, y1))
+
+
+
+# Initial Form 
+tk = Tk()
+tk.title('assignment1')
+tk.resizable(0, 0)
+
+
+#Intial Value
+width = 500
+height = 500
+
 multiply_x = width/60
 multiply_y = height/60
-first_x = int(width/4)
-first_y = int(height/1.1)
+zero_x = int(width/4)
+zero_y = int(height/1.1)
+car_x = zero_x
+car_y = zero_y
+
+car = car(zero_x, zero_y, multiply_x, multiply_y, car_x, car_y)
+
 canvas = Canvas(tk, width=width, height=height, bg='ivory')
 canvas.pack(fill=BOTH, expand=1)
+canvas.pack()
+# WALL
+car.create_line(-6, -3, -6, 22, "blue")
+car.create_line(-6, 22, 18, 22, "blue")
+car.create_line(18, 22, 18, 50, "blue")
+car.create_line(18, 50, 30, 50, "blue")
+car.create_line(30, 50, 30, 10, "blue")
+car.create_line(30, 10, 6, 10, "blue")
+car.create_line(6, 10, 6, -3, "blue")
+car.create_line(6, -3, -6, -3, "blue")
+
+# GOAL Line
+car.create_line(18, 40, 30, 40, "red")
+
+
+
+# CAR
+car_obj = car.create_circle()
+""" car.create_line(-12, 0, 12, 0, "black")
+car.create_line(0, 0, 0, 4, "red")
+car.create_line(0, 0, 0, -4, "black") """
+
 while 1:
-    canvas.pack()  # 默认布局
-    ##WALL
-    create_line(-6,-3,-6,22,"blue")
-    create_line(-6,22,18,22,"blue")
-    create_line(18,22,18,50,"blue")
-    create_line(18,50,30,50,"blue")
-    create_line(30,50,30,10,"blue")
-    create_line(30,10,6,10,"blue")
-    create_line(6,10,6,-3,"blue")
-    create_line(6,-3,-6,-3,"blue")
+    """ car.car_move(canvas, car_obj)
+    time.sleep(0.5) """
+    theta = int(input('Input the theta between +- 40: \n'))
+    if(theta == -1 ):
+        break
 
-    ##GOAL
-    create_line(18,45,30,45,"red")
+    car.car_move(canvas, car_obj, theta)
 
-    ##CAR
-    create_circle(first_x, first_y, 2, "red")
-    create_line(-12,0,12,0,"black")
-    create_line(0,0,0,4,"red")
-    create_line(0,0,0,-4,"black")
-    
-    # height = tk.winfo_screenheight() 
-    # width = tk.winfo_screenwidth() 
     tk.update()
     canvas.update()
 
@@ -53,7 +116,6 @@ while 1:
 # tank.create_line(30, 300, 570, 300, width=1.5, fill='red', dash=6)  # 网格,虚线
 # tank.create_line(300, 30, 300, 570, width=1.5, fill='red', dash=6)
 # id_obj = tank.create_oval(130, 130, 270, 270, width=0, fill='gray')  # 物体
-
 
 # def move_obj(tank, idx, pos):  # 移动坐标
 
@@ -67,7 +129,6 @@ while 1:
 #         tank.coords(idx, (130, 330, 270, 470))
 #     else:
 #         return 0
-
 
 # while 1:
 #     move_obj(tank, id_obj, 1)
