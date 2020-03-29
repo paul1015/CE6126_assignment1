40ecf28e431a872b3ee8473c3c039e6a5ac8aba1
chungpaul <chungpaul1015@gmail.com>
2020-03-29 21:48:35 +0800

    sensor fail

--- code/car.py
+++ code/car.py
@@ -18,18 +18,40 @@ class car:
         self.car_r = car_r
         self.car_collar = car_collar
         self.car_angle = round(math.radians(car_angle),2)
+        self.direction = 0
+        self.ld = 0
+        self.rd = 0
+        self.d = 0
+        self.b = car_r * multiply_x
+        self.x1 = zero_x + -6 * multiply_x
+        self.x2 = zero_x + 6 * multiply_x
+        self.x3 = zero_x + 18 * multiply_x
+        self.x4 = zero_x + 30 * multiply_x
+        self.y1 = zero_y + -10 * multiply_y
+        self.y2 = zero_y + -22* multiply_y
+        self.y3 = zero_y + -30 * multiply_y
 
 
     def create_line(self, x1, y1, x2, y2, color):
-        canvas.create_line(zero_x+x1*multiply_x, zero_y+(y1*-1)*self.multiply_y,
+        return canvas.create_line(zero_x+x1*multiply_x, zero_y+(y1*-1)*self.multiply_y,
                            zero_x+x2*multiply_x, zero_y+(y2*-1)*self.multiply_y, fill=color, width=3)
 
-    def create_circle(self):  
+    def create_car(self):  
+        
         # center coordinates, radius
+
+        """ x0 = self.zero_x + self.car_x * self.multiply_x - self.car_r*self.multiply_x
+        y0 = self.zero_y + (self.car_y * -1 ) * self.multiply_y - self.car_r*self.multiply_y
+        x1 = self.zero_x + self.car_x * self.multiply_x + self.car_r*self.multiply_x
+        y1 = self.zero_y + (self.car_y * -1 ) *self.multiply_y + self.car_r*self.multiply_y """
+
         x0 = self.car_x - self.car_r*self.multiply_x
         y0 = self.car_y - self.car_r*self.multiply_y
         x1 = self.car_x + self.car_r*self.multiply_x
         y1 = self.car_y + self.car_r*self.multiply_y
+
+        
+
         return canvas.create_oval(x0, y0, x1, y1, fill=self.car_collar)
 
 
@@ -44,7 +66,6 @@ class car:
         #self.car_y = self.car_y - (math.sin(self.car_angle + theta) - (math.sin(theta)) * math.cos(self.car_angle))
         #self.car_angle = self.car_angle - math.asin(2 * math.sin(theta) / self.car_r)
         self.car_x = self.car_x + 10*(math.cos(self.car_angle) * math.cos(theta)) 
-        
         self.car_y = self.car_y -  10*(math.sin(self.car_angle) * math.cos(theta))
         self.car_angle = self.car_angle - math.asin(2 * math.sin(theta) / self.car_r)
 
@@ -54,8 +75,49 @@ class car:
         y0 = self.car_y - self.car_r*self.multiply_y
         x1 = self.car_x + self.car_r*self.multiply_x
         y1 = self.car_y + self.car_r*self.multiply_y
+        
+        #draw the car
         tk.coords(car, (x0, y0, x1, y1))
 
+        canvas.delete(self.direction)
+        # direction line 
+        #car_x = zero_x + car_x * multiply_x
+        #car_y = zero_y + (car_y * -1 )*multiply_y
+        car_x1 =  (self.car_x - self.zero_x) / self.multiply_x
+        car_y1 = (self.car_y  - self.zero_y) / self.multiply_y * -1
+        
+        
+        x2 = car_x1 + 6* math.cos(self.car_angle)
+        y2 = car_y1 + 6* math.sin(self.car_angle)
+
+        self.direction = self.create_line(car_x1, car_y1, x2, y2, "red")
+
+        print('car_d', self.car_x, self.x1, self.x2, self.car_y, self.y1)
+
+        langle = self.car_angle - round(math.radians(45),2)
+        rangle = self.car_angle + round(math.radians(45),2)
+        #分四塊去運算
+        if(self.car_x >= self.x1 and self.car_x <= self.x2 and self.car_y >= self.y1):
+            d11 = abs(self.car_y - self.y2) * (1/ math.sin(self.car_angle))
+            d12 = abs(self.car_x - self.x1 ) * (1/ math.cos(self.car_angle))
+            d13 = abs(self.car_x - self.x2 ) * (1/ math.cos(self.car_angle))
+            self.d = min(d11, d12, d13,) 
+
+            ld1 = abs(self.car_y - self.y2) * (1/ math.sin(langle))
+            ld2 = abs(self.car_x - self.x1 ) * (1/ math.cos(langle))
+            ld3 = abs(self.car_x - self.x2 ) * (1/ math.cos(langle))
+            self.ld = min(ld1, ld2, ld3) 
+
+            c = ((self.x2 - self.car_x) * (self.x2 - self.car_x) - (self.y1 - self.car_y) *  (self.y1 - self.car_y)) * 0.5
+            rd1 = abs(self.car_y - self.y2) * (1/ math.sin(rangle))
+            rd2 =  abs(self.car_x - self.x1 ) * (1/ math.cos(rangle))
+            rd3 = abs(self.car_x - self.x2 ) * (1/ math.cos(rangle))
+            self.rd = min(abs(self.car_y - self.y2) * (1/ math.sin(rangle)),  
+                     abs(self.car_x - self.x1 ) * (1/ math.cos(rangle)),
+                     abs(self.car_x - self.x2 ) * (1/ math.cos(rangle)),
+                    ) 
+            print('self.d ', self.d, self.ld, self.rd )
+        
 
 
 # Initial Form 
@@ -112,33 +174,17 @@ for line in f:
         print('goal', x1, y1, x2, y2)
         car.create_line(x1, y1, x2, y2, "red")
 
-    if(i > 2):
-        if(i != 3):
-            car.create_line(x1, y1, x2, y2, "blue")
+    if(i > 3):
+        car.create_line(x1, y1, x2, y2, "blue")
 
     i = i + 1
-    
-car.create_line(-12, 0, 12, 0, "black")
-
-# WALL
-""" car.create_line(-6, -3, -6, 22, "blue")
-car.create_line(-6, 22, 18, 22, "blue")
-car.create_line(18, 22, 18, 50, "blue")
-car.create_line(18, 50, 30, 50, "blue")
-car.create_line(30, 50, 30, 10, "blue")
-car.create_line(30, 10, 6, 10, "blue")
-car.create_line(6, 10, 6, -3, "blue")
-car.create_line(6, -3, -6, -3, "blue") """
-
-# GOAL Line
-#car.create_line(18, 40, 30, 30, "red")
-
 
 # CAR
-car_obj = car.create_circle()
-""" car.create_line(-12, 0, 12, 0, "black")
-car.create_line(0, 0, 0, 4, "red")
-car.create_line(0, 0, 0, -4, "black") """
+car_obj = car.create_car()
+
+#起跑線 0,0
+car.create_line(-12, 0, 12, 0, "black")
+
 
 while 1:
     """ car.car_move(canvas, car_obj)
